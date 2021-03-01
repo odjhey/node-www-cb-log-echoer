4dde6939adce0c78e4b1ad3341ce7fd77b85a2cd
odee <odee@gmail.com>
2021-03-01 20:55:07 +0000

    narrow down logged vars

--- server.js
+++ server.js
@@ -1,21 +1,59 @@
 require("dotenv").config();
 //server.js
 const express = require("express");
+const bodyParser = require("body-parser");
 //const favicon = require('express-favicon');
 const port = process.env.PORT || 7777;
 const app = express();
+
+
+/*
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+*/
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get("/", function (req, res) {
   return res.send("hello brotha!");
 });
 
 app.post("*", function (req, res, next) {
-	console.log("post", req)
+	console.log("post", {
+		baseUrl: req.baseUrl,
+		originalUrl: req.originalUrl,
+		body: req.body,
+		hostname: req.hostname,
+		headers: req.headers,
+		rewHeaders: req.rawHeaders,
+		ip: req.ip,
+		ips: req.ips,
+		method: req.method,
+		params: req.params,
+		path: req.path,
+		query: req.query,
+		route: req.route,
+	} )
 	next()
 });
 
 app.get("*", function (req, res, next) {
-	console.log("get", req)
+	console.log("get", {
+		baseUrl: req.baseUrl,
+		originalUrl: req.originalUrl,
+		body: req.body,
+		hostname: req.hostname,
+		headers: req.headers,
+		rewHeaders: req.rawHeaders,
+		ip: req.ip,
+		ips: req.ips,
+		method: req.method,
+		params: req.params,
+		path: req.path,
+		query: req.query,
+		route: req.route,
+	} )
 	next()
 });
 
